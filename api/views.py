568be0252f4bccafa2feb269845c2fb8f49782b4
mio256 <reiya@woorth.io>
2023-04-14 18:22:44 +0900

    :fire: EthBlockViewSet.cache_blocks()

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from api import alchemy, models, serializers
+import itertools
+from concurrent.futures import ThreadPoolExecutor
+from django.http import JsonResponse
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from api import models, serializers
+from rest_framework import viewsets, permissions, decorators
+
+# settings
+blockTransactionDetail = False
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -27,7 +29,37 @@ class GroupViewSet(viewsets.ModelViewSet):
 
 class EthBlockViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    TODO
     """
     queryset = models.EthBlock.objects.all()
+    api = alchemy.EthApiClass()
+    model = models.EthBlock
     serializer_class = serializers.EthBlockSerializer
+
+    def saveBlock(self, response: dict):
+        smartblock = self.model(
+            number=response['number'],
+            hash=response['hash'],
+            timestamp=response['timestamp'],
+            transactions=response['transactions']
+        )
+        smartblock.save()
+
+    @decorators.action(detail=False)
+    def cache_blocks(self, request):
+        latest_number = self.api.getNumber()
+        try:
+            latest_object = self.model.objects.first().number
+            if int(latest_number, 16) - int(latest_object, 16) > 128:
+                raise ValueError
+        except (AttributeError, ValueError) as e:  # notExistsObject or tooManyObject
+            response = self.api.getFormatBlock(latest_number, blockTransactionDetail)
+            self.saveBlock(response)
+            return JsonResponse({'process': f'{self}', 'error': e.__class__.__name__, 'count': 1, 'latest_number': latest_number})
+
+        rge = [hex(x) for x in range(int(latest_object, 16) + 1, int(latest_number, 16) + 1)]
+        with ThreadPoolExecutor() as executor:
+            responses = executor.map(self.api.getFormatBlock, rge, itertools.repeat(blockTransactionDetail))
+        for response in responses:
+            self.saveBlock(response)
+        return JsonResponse({'process': f'{self}', 'count': len(rge), 'latest_number': latest_number, 'latest_object': latest_object})
